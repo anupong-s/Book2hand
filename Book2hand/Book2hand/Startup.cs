8020b8c16d8a3d03276a5d7a9cf884aed7756f91
anupong.s@carpass.co.th <anupong.s@carpass.co.th>
2014-08-10 22:17:26 +0700

    - Create new project and use vs 2013 and bootstrap v3.2

---
+++ Book2hand/Book2hand/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Book2hand.Startup))]
+namespace Book2hand
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
